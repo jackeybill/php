52f164aa9ee4f4d873ba8754f853dd7cc1f5dc49
xuej <xuejun@yifeiyang.net>
2015-07-06 16:08:05 +0800

    new files for the anglarJS example

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -2,5 +2,6 @@ var app = angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.filters',
-  'myApp.controllers'
+  'myApp.controllers',
+  'myApp.FirstController'
   ]);
--- angularjs/js/controllers.js
+++ angularjs/js/controllers.js
@@ -11,3 +11,10 @@ angular.module('myApp.controllers', [])
   }, 1000);
   updateClock();
 });
+
+angular.module('myApp.FirstController', [])
+.controller('FirstController', ['$scope', function($scope) {
+    $scope.counter = 0;
+    $scope.add = function(amount) { $scope.counter += amount; };
+    $scope.subtract = function(amount) { $scope.counter -= amount; };
+}]);
